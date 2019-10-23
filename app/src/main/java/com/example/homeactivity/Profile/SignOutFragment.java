426db8de5709fda956a88993ca90ca128eb83397
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-10-23 22:11:29 +0900

    회원가입 Firebase 연동중. ~part28

--- app/src/main/java/com/example/homeactivity/Profile/SignOutFragment.java
+++ app/src/main/java/com/example/homeactivity/Profile/SignOutFragment.java
@@ -1,9 +1,12 @@
 package com.example.homeactivity.Profile;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -11,25 +14,98 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.homeactivity.Login.LoginActivity;
 import com.example.homeactivity.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignOutFragment extends Fragment {
     private static final String TAG = "HomeFragment" ;
 
     //firebase
     private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListner;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     private ProgressBar mProgressBar;
-    private TextView tvSignout;
+    private TextView tvSignout,tvSigningOut;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_signout, container, false);
+        tvSignout = (TextView) view.findViewById(R.id.tvConfirmSignout);
+        mProgressBar = (ProgressBar) view.findViewById(R.id.progressBar);
+        tvSigningOut = (TextView) view.findViewById(R.id.btnConfirmSignout);
+        Button btnConfirmSignout = (Button) view.findViewById(R.id.btnConfirmSignout);
+
+        mProgressBar.setVisibility(View.GONE);
+        tvSignout.setVisibility(View.GONE);
+
+        setupFirebaseAuth();
+
+        btnConfirmSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to sign out.");
+                mProgressBar.setVisibility(View.VISIBLE);
+                tvSignout.setVisibility(View.VISIBLE);
+
+                mAuth.signOut();
+                getActivity().finish();
+            }
+        });
 
         return  view ;
     }
+
+
+    /*
+============================================= Firebase =================================================
+ */
+
+    /**
+     *  Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:"+ user.getUid());
+                }else {
+                    // User is singed out
+                    Log.d(TAG, "onAuthStateChanged: singed_out");
+
+                    Log.d(TAG, "onAuthStateChanged: navigating back to login screen.");
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+                //...
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
