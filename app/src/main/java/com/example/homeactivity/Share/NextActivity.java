c2f733d06a95d798ab9699e53cd38cbd757c647d
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-10-29 21:59:40 +0900

    ~part57 오류 수정중.(버전 맞추기.)

---
+++ app/src/main/java/com/example/homeactivity/Share/NextActivity.java
@@ -0,0 +1,172 @@
+package com.example.homeactivity.Share;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.homeactivity.R;
+import com.example.homeactivity.Utils.FirebaseMethods;
+import com.example.homeactivity.Utils.UniversalImageLoader;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+/**
+ * Created by Mingeon on 29/10/2019.
+ */
+
+public class NextActivity extends AppCompatActivity {
+
+    private static final String TAG = "NextActivity";
+
+    //firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
+    private FirebaseMethods mFirebaseMethods;
+
+    //widgets
+    private EditText mCaption;
+
+    //vars
+    private String mAppend = "file:/";
+    private int imageCount = 0;
+    private String imgUrl;
+    private Intent intent;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        mFirebaseMethods = new FirebaseMethods(NextActivity.this);
+        mCaption = (EditText) findViewById(R.id.caption);
+
+        setupFirebaseAuth();
+
+        ImageView backArrow = (ImageView) findViewById(R.id.ivBackArrow);
+        backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: closing the activity.");
+                finish();
+            }
+        });
+
+        TextView share = (TextView) findViewById(R.id.tvShare);
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: navigating to the final share screen.");
+                // upload the image to firebase
+                Toast.makeText(NextActivity.this,"Attempting to upload new photo", Toast.LENGTH_SHORT).show();
+                String caption = mCaption.getText().toString();
+                mFirebaseMethods.uploadNewPhoto(getString(R.string.new_photo), caption, imageCount, imgUrl);
+            }
+        });
+
+        setImage();
+    }
+
+    private void someMethod(){
+        /**
+         *  step 1)
+         *  Create a data model for Photos
+         *
+         *  step 2)
+         *  Add properties to the Photo Objects (caption, date, imageUrl, photo_id, tags, user_id)
+         *
+         *  step 3)
+         *  Count the number of photos that the user already has.
+         *
+         *  step 4)
+         *  a) Upload the photo to Firebase Storage
+         *  b) insert into 'photos' node
+         *  c) insert ito 'user_photos' node
+         */
+
+    }
+
+    /**
+     *  gets the image url from the incoming intent and displays the chosen image
+     */
+    private  void setImage(){
+        intent = getIntent();
+        ImageView image = (ImageView) findViewById(R.id.imageShare);
+        imgUrl = intent.getStringExtra(getString(R.string.selected_image));
+        UniversalImageLoader.setImage(imgUrl, image, null, mAppend);
+    }
+
+       /*
+============================================= Firebase =================================================
+ */
+
+    /**
+     *  Setup the firebase auth object
+     */
+    private void setupFirebaseAuth(){
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
+        Log.d(TAG, "onDataChange: image count: " + imageCount);
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if(user != null){
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged: signed_in:"+ user.getUid());
+                }else {
+                    // User is singed out
+                    Log.d(TAG, "onAuthStateChanged: singed_out");
+                }
+                //...
+            }
+        };
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+
+                imageCount = mFirebaseMethods.getImageCount(dataSnapshot);
+                Log.d(TAG, "onDataChange: image count: " + imageCount);
+            }
+
+            @Override
+            public void onCancelled( DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+}
