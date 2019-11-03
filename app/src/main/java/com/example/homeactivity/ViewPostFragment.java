47c5c09e3884450d5b4d1611c28fafe9cf2f40e9
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-11-04 01:52:05 +0900

    ~part 66

--- app/src/main/java/com/example/homeactivity/ViewPostFragment.java
+++ app/src/main/java/com/example/homeactivity/ViewPostFragment.java
@@ -1,13 +1,25 @@
 package com.example.homeactivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.homeactivity.Utils.BottomNavigationViewHelper;
+import com.example.homeactivity.Utils.SquareImageView;
+import com.example.homeactivity.Utils.UniversalImageLoader;
+import com.example.homeactivity.models.Photo;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
 /**
  * Created by Mingeon on 04/11/2019.
@@ -15,11 +27,92 @@ import androidx.fragment.app.Fragment;
 
 public class ViewPostFragment extends Fragment {
 
+    private static final String TAG = "ViewPostFragment";
+
+    public ViewPostFragment() {
+        super();
+        setArguments(new Bundle());
+    }
+
+    //widgets
+    private SquareImageView mPostImage;
+    private BottomNavigationViewEx bottomNavigationView;
+    private TextView mBackLabel, mCaption, mUsername, mTimestamp;
+    private ImageView mBackArrow, mEllipses, mHeartRed, mHeartWhite, mProfileImage;
+
+    //vars
+    private Photo mPhoto;
+    private int mActivityNumber = 0;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_view_post, container, false);
+        mPostImage = (SquareImageView) view.findViewById(R.id.post_image);
+        bottomNavigationView = (BottomNavigationViewEx) view.findViewById(R.id.bottomNavViewBar);
+        mBackArrow = (ImageView) view.findViewById(R.id.backArrow);
+        mBackLabel = (TextView) view.findViewById(R.id.tvBackLabel);
+        mCaption = (TextView) view.findViewById(R.id.image_caption);
+        mUsername = (TextView) view.findViewById(R.id.username);
+        mTimestamp = (TextView) view.findViewById(R.id.image_time_posted);
+        mEllipses = (ImageView) view.findViewById(R.id.ivEllipses);
+        mHeartRed = (ImageView) view.findViewById(R.id.image_heart_red);
+        mHeartWhite = (ImageView) view.findViewById(R.id.image_heart);
+        mProfileImage = (ImageView) view.findViewById(R.id.profile_photo);
+
+        try {
+            mPhoto = getPhotoFromBundle();
+            UniversalImageLoader.setImage(mPhoto.getImage_path(), mPostImage, null, "");
+            mActivityNumber = getActivityNumFromBundle();
+
+        } catch (NullPointerException e) {
+            Log.e(TAG, "onCreateView: NullPointerException: " + e.getMessage());
+        }
+
+        setupBotttomNavigationView();
 
         return view;
+    }
+
+    /**
+     * retrieve the activity number from the incoming bundle from profileActivity interface
+     * @return
+     */
+    private int getActivityNumFromBundle() {
+        Log.d(TAG, "getActivityNumFromBundle: arguments: " + getArguments());
+
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            return bundle.getInt(getString(R.string.activity_number));
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * retrieve the photo from the incoming bundle from profileActivity interface
+     * @return
+     */
+    private Photo getPhotoFromBundle() {
+        Log.d(TAG, "getPhotoFromBundle: arguments: " + getArguments());
+
+        Bundle bundle = this.getArguments();
+        if (bundle != null) {
+            return bundle.getParcelable(getString(R.string.photo));
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBotttomNavigationView() {
+        Log.d(TAG, "setupBotttomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationView);
+        BottomNavigationViewHelper.enableNavigation(getActivity(), getActivity() ,bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(mActivityNumber);
+        menuItem.setChecked(true);
     }
 }
