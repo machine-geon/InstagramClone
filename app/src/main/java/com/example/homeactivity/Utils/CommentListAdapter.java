e31f51e4b48f9a92a3c56212487b718d14933ea1
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-11-05 23:42:21 +0900

    ~part 76

---
+++ app/src/main/java/com/example/homeactivity/Utils/CommentListAdapter.java
@@ -0,0 +1,120 @@
+package com.example.homeactivity.Utils;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.homeactivity.R;
+import com.example.homeactivity.models.Comment;
+
+import org.w3c.dom.Text;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by Mingeon on 05/11/2019.
+ */
+
+public class CommentListAdapter extends ArrayAdapter<Comment> {
+
+    private static final String TAG = "CommentListAdapter";
+
+    private LayoutInflater mInflater;
+    private int layoutResource;
+    private Context mContext;
+
+    public CommentListAdapter(@NonNull Context context, int resource, @NonNull List<Comment> objects) {
+        super(context, resource, objects);
+        mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mContext = context;
+        layoutResource = resource;
+    }
+
+    private static class ViewHolder{
+        TextView commnet, username, timestamp, reply, likes;
+        CircleImageView profileImage;
+        ImageView like;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final ViewHolder holder;
+
+        if(convertView == null){
+            convertView = mInflater.inflate(layoutResource, parent, false);
+            holder = new ViewHolder();
+
+            holder.commnet = (TextView) convertView.findViewById(R.id.comment);
+            holder.username = (TextView) convertView.findViewById(R.id.comment_username);
+            holder.timestamp = (TextView) convertView.findViewById(R.id.comment_time_posted);
+            holder.reply = (TextView) convertView.findViewById(R.id.comment_reply);
+            holder.like = (ImageView) convertView.findViewById(R.id.comment_like);
+            holder.profileImage = (CircleImageView) convertView.findViewById(R.id.comment_profile_image);
+
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        //set the comment
+        holder.commnet.setText(getItem(position).getComment());
+
+        //set the timestamp difference
+        String timestampDifference = getTimestampDifference(getItem(position));
+        if(!timestampDifference.equals("0")){
+            holder.timestamp.setText(timestampDifference + "일");
+        }else {
+            holder.timestamp.setText("오늘");
+        }
+
+        //set the username
+
+
+        return convertView;
+    }
+
+    /**
+     * Returns a string representing the number of days ago the post was made
+     *
+     * @return
+     */
+    private String getTimestampDifference(Comment comment) {
+        Log.d(TAG, "getTimestampDifference: ");
+
+        String difference = "";
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.KOREA);
+        sdf.setTimeZone(TimeZone.getTimeZone("Asia/Seoul")); //google "android list of timezones'
+        Date today = c.getTime();
+        sdf.format(today);
+        Date timestamp;
+        final String photoTimestamp = comment.getDate_created();
+        try {
+            timestamp = sdf.parse(photoTimestamp);
+            difference = String.valueOf(Math.round(((today.getTime() - timestamp.getTime()) / 1000 / 60 / 60 / 24)));
+        } catch (ParseException e) {
+            Log.e(TAG, "getTimestampDifference: ParseException: " + e.getMessage());
+            difference = "0";
+        }
+        return difference;
+    }
+}
