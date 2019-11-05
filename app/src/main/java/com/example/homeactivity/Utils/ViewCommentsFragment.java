04280c57a08dbe878a7bbd6cf204be2538cea37a
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-11-05 23:06:08 +0900

    ~part 74

---
+++ app/src/main/java/com/example/homeactivity/Utils/ViewCommentsFragment.java
@@ -0,0 +1,57 @@
+package com.example.homeactivity.Utils;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.homeactivity.R;
+import com.example.homeactivity.models.Like;
+import com.example.homeactivity.models.Photo;
+import com.example.homeactivity.models.User;
+import com.example.homeactivity.models.UserAccountSettings;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Created by Mingeon on 04/11/2019.
+ */
+
+public class ViewCommentsFragment extends Fragment {
+
+    private static final String TAG = "ViewCommentsFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_view_comments, container, false);
+
+        return view;
+    }
+
+}
