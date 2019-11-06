ae1716b94e390be6ac2a357c943b5a5917b1e99b
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-11-06 15:11:19 +0900

    ~part 83 backArrow 부분 NullPoint error 보류.(주석처리)

--- app/src/main/java/com/example/homeactivity/Search/SearchActivity.java
+++ app/src/main/java/com/example/homeactivity/Search/SearchActivity.java
@@ -2,11 +2,14 @@ package com.example.homeactivity.Search;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -16,6 +19,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.homeactivity.R;
 import com.example.homeactivity.Utils.BottomNavigationViewHelper;
+import com.example.homeactivity.Utils.UserListAdapter;
 import com.example.homeactivity.models.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -25,7 +29,9 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
     private static final String TAG = "SearchActivity";
@@ -39,35 +45,65 @@ public class SearchActivity extends AppCompatActivity {
 
     //vars
     private List<User> mUserList;
+    private UserListAdapter mAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        mSearchParam = (EditText) findViewById(R.id.search);
+        mListView = (ListView) findViewById(R.id.listView);
         Log.d(TAG, "onCreate: starting.");
 
         hideSoftKeyboard();
         setupBotttomNavigationView();
+        initTextListener();
     }
 
-    private void searchForMatch(String keyword){
+    private void initTextListener() {
+        Log.d(TAG, "initTextListener: initializing");
+
+        mUserList = new ArrayList<>();
+
+        mSearchParam.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                String text = mSearchParam.getText().toString().toLowerCase(Locale.getDefault());
+                searchForMatch(text);
+            }
+        });
+    }
+
+    private void searchForMatch(String keyword) {
         Log.d(TAG, "searchForMatch: searching for a match: " + keyword);
         mUserList.clear();
         //update the users list
-        if(keyword.length() == 0){
+        if (keyword.length() == 0) {
 
-        }else {
+        } else {
             DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
             Query query = reference.child(getString(R.string.dbname_users))
                     .orderByChild(getString(R.string.field_username)).equalTo(keyword);
             query.addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                    for (DataSnapshot singleSnapshot : dataSnapshot.getChildren()) {
                         Log.d(TAG, "onDataChange: found user: " + singleSnapshot.getValue(User.class).toString());
 
                         mUserList.add(singleSnapshot.getValue(User.class));
                         //update the users list view
+                        updateUserList();
                     }
                 }
 
@@ -77,6 +113,24 @@ public class SearchActivity extends AppCompatActivity {
                 }
             });
         }
+    }
+
+    private void updateUserList(){
+        Log.d(TAG, "updateUserList: updating users list");
+
+        mAdapter = new UserListAdapter(SearchActivity.this, R.layout.layout_user_listitem, mUserList);
+
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "onItemClick: selected user: " + mUserList.get(position).toString());
+
+                //navigate to profile activity
+
+            }
+        });
     }
 
     private void hideSoftKeyboard() {
