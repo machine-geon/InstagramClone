7e39a398166ce55140f7340a5cec3a83730dce30
KimMingeon0625 <55868455+KimMingeon0625@users.noreply.github.com>
2019-11-06 14:44:55 +0900

    ~part 82 backArrow 부분 NullPoint error 보류.(주석처리)

--- app/src/main/java/com/example/homeactivity/Search/SearchActivity.java
+++ app/src/main/java/com/example/homeactivity/Search/SearchActivity.java
@@ -5,37 +5,95 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.homeactivity.R;
 import com.example.homeactivity.Utils.BottomNavigationViewHelper;
+import com.example.homeactivity.models.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class SearchActivity extends AppCompatActivity {
-    private static final String TAG = "SearchActivity" ;
-    private static final int ACTIVITY_NUM = 1 ;
+import java.util.List;
 
-    private Context mContext = SearchActivity.this ;
+public class SearchActivity extends AppCompatActivity {
+    private static final String TAG = "SearchActivity";
+    private static final int ACTIVITY_NUM = 1;
+
+    private Context mContext = SearchActivity.this;
+
+    //widget
+    private EditText mSearchParam;
+    private ListView mListView;
+
+    //vars
+    private List<User> mUserList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
         Log.d(TAG, "onCreate: starting.");
 
+        hideSoftKeyboard();
         setupBotttomNavigationView();
     }
 
+    private void searchForMatch(String keyword){
+        Log.d(TAG, "searchForMatch: searching for a match: " + keyword);
+        mUserList.clear();
+        //update the users list
+        if(keyword.length() == 0){
+
+        }else {
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+            Query query = reference.child(getString(R.string.dbname_users))
+                    .orderByChild(getString(R.string.field_username)).equalTo(keyword);
+            query.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    for(DataSnapshot singleSnapshot : dataSnapshot.getChildren()){
+                        Log.d(TAG, "onDataChange: found user: " + singleSnapshot.getValue(User.class).toString());
+
+                        mUserList.add(singleSnapshot.getValue(User.class));
+                        //update the users list view
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+        }
+    }
+
+    private void hideSoftKeyboard() {
+        if (getCurrentFocus() != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+        }
+    }
+
     //BottomNavigationView setup
-    private void setupBotttomNavigationView(){
-        Log.d(TAG, "setupBotttomNavigationView: setting up BottomNavigationView") ;
-        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar) ;
-        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx) ;
-        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx) ;
-        Menu menu = bottomNavigationViewEx.getMenu() ;
-        MenuItem menuItem = menu.getItem(ACTIVITY_NUM) ;
-        menuItem.setChecked(true) ;
+    private void setupBotttomNavigationView() {
+        Log.d(TAG, "setupBotttomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewHelper.enableNavigation(mContext, this, bottomNavigationViewEx);
+        Menu menu = bottomNavigationViewEx.getMenu();
+        MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
+        menuItem.setChecked(true);
     }
 }
